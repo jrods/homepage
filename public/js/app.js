0b793c272ff0bbe10a499b880b96f1ba23930921
Jared Smith <jared.smith.jrod@gmail.com>
2015-09-30 15:42:49 -0700

    adjusting some things from feedback

--- public/js/app.js
+++ public/js/app.js
@@ -3,10 +3,4 @@ window.addEventListener('DOMContentLoaded', function (event) {
 
 	stickyElement('#nav', 0);
 
-	stickyElement('#about h2', 52);
-
-	stickyElement('#contact h2', 104);
-
-	stickyElement('#skills h2', 156);
-
 });
